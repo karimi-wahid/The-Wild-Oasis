cb2b02b506a846448277d045bf1471fca53cd329
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-06-17 09:21:48 +0430

    Add global styles, layout updates, and new data service functions; include images

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,27 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Logo from "@/components/Logo";
+import { Josefin_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "The wild oasis website.",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome /  The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Afghanistan, Kabul surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo />
         </header>
