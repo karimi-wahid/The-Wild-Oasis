5fefe703e88c3601f7f6948e0fa6810ccbe777ca
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-06-18 08:53:03 +0430

    Add Header and Navigation components; update layout and home page with new structure and styles

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Logo from "@/components/Logo";
 import { Josefin_Sans } from "next/font/google";
+import Header from "../components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -21,11 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-        </header>
-        {children}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
