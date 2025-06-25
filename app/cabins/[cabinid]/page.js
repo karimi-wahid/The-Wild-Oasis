954e2d536ba726e2aa6e62b62991f5bdcfe903c9
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-06-25 20:45:50 +0430

    Refactor cabin page for generateStaticParams

--- app/cabins/[cabinid]/page.js
+++ app/cabins/[cabinid]/page.js
@@ -7,6 +7,14 @@ export async function generateMetadata({ params }) {
   return { title: name };
 }
 
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinid);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
