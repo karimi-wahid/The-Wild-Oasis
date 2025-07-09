86e8ae99e2712f78e21b92245f3fa16289f6e835
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-07-09 09:18:36 +0430

    Add LoginMessage component and integrate authentication in Reservation and ReservationForm components

--- components/Reservation.js
+++ components/Reservation.js
@@ -2,12 +2,16 @@ import React from "react";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "@/lib/data-service";
+import { auth } from "@/lib/auth";
+import LoginMessage from "./LoginMessage";
 
-export default function Reservation({ cabin }) {
+export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px] mb-10 text-accent-400">
       <DateSelector
@@ -15,7 +19,11 @@ export default function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
